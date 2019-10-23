f9959ef2a733cb31eb631392737181d50603e5cc
james-truong <james.truong@hotmail.com>
2019-10-23 04:11:28 +0000

    added keys to move snake! and refactored logic into module

--- client.js
+++ client.js
@@ -2,20 +2,21 @@ const net = require("net");
 //const { IP, PORT } = require('./constants');
 
 // establishes connection with game server
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
- 
+
   // notifies to console when connection is made to server and sends name
   conn.on('connect', () => {
     console.log("Connection established, name sent");
     conn.write("Name: JT");
+    //setInterval(() => conn.write("Move: up"), 1000);
   });
-  
+
   // handle messages incoming from server
   conn.on('data', (data) => {
     console.log('Server says: ', data);
